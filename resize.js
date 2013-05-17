aa861fdd401a4cbd7b91cc6c8fa8663d32ac6a22
Allan Neil <allan@allanneil.com>
2013-05-17 03:23:11 -0400

    A bunch of changes.

--- resize.js
+++ resize.js
@@ -9,12 +9,12 @@ function updateContainer() {
   $(window).resize(function() {
     var goldenWidth  = windowWidth  = $(this).width(),
         goldenHeight = windowHeight = $(this).height(),
-		h1Size = $(this).width() / 40 ;
+		h1Size = $(this).width() / 45 ;
 
 $("h1").css({
   fontSize: h1Size
 });
-$("h2").css({
+$("h3").css({
   fontSize: h1Size * 0.62
 });
 
@@ -22,8 +22,33 @@ $("p").css({
   fontSize: h1Size * 0.62 * 0.62
 });
 
+ $("#wayfinder_eye_black").css({
+	height: goldenWidth * 0.03 * 0.75, width: goldenWidth * 0.03
+}); 
+
+ $("#wayfinder_compass_black").css({
+	height: goldenWidth * 0.025, width: goldenWidth * 0.025
+}); 
+
+ $("#wayfinder_gear_black").css({
+	height: goldenWidth * 0.025, width: goldenWidth * 0.025
+}); 
+
+
  $("#wayfinder_logo_dog").css({
 	height: goldenWidth * 0.15 * 0.6, width: goldenWidth * 0.15
+}); 
+
+ $("#wayfinder_eye").css({
+	height: goldenWidth * 0.023 * 0.75, width: goldenWidth * 0.023
+}); 
+
+$("#wayfinder_gear_green").css({
+	height: goldenWidth * 0.07, width: goldenWidth * 0.07
+}); 
+
+$("#wayfinder_compass_purple").css({
+	height: goldenWidth * 0.039, width: goldenWidth * 0.039
 }); 
 
     if (windowWidth/windowHeight > phi) {
